d8ec69492cfc24256a0816d127209a70d0f8de3e
goktug <goktug@spykegames.com>
2022-07-20 10:23:18 +0300

    dont exit on any errors

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # Exit on any errors
+#set -e # Exit on any errors
 
 # Get the directory of this script:
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
